0662e50cc8c84a25fabbeea3d05cad362cc04347
Oleg <oydemidov@gmail.com>
2019-01-17 14:36:57 +0300

    Authenticating window is working now

--- src/main/java/ru/avem/posum/controllers/LoginController.java
+++ src/main/java/ru/avem/posum/controllers/LoginController.java
@@ -1,14 +1,20 @@
 package ru.avem.posum.controllers;
 
 import javafx.fxml.FXML;
-import ru.avem.posum.Main;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import ru.avem.posum.Main;
+import ru.avem.posum.db.AccountRepository;
+import ru.avem.posum.db.models.Account;
+import ru.avem.posum.utils.Toast;
+
+import java.util.List;
 
 public class LoginController {
     @FXML
     private TextField userLogin;
     @FXML
-    private TextField userPassword;
+    private PasswordField userPassword;
 
     private Main main;
 
@@ -17,6 +23,30 @@ public class LoginController {
     }
 
     public void handleLogIn() {
-        
+        authenticateUser();
+    }
+
+    public void authenticateUser() {
+        List<Account> allAccounts = AccountRepository.getAllAccounts();
+        String login = userLogin.getText();
+        String password = userPassword.getText();
+
+        if (login.isEmpty()) {
+            Toast.makeText("Введите имя пользователя").show(Toast.ToastType.WARNING);
+        } else if (password.isEmpty()) {
+            Toast.makeText("Введите пароль").show(Toast.ToastType.WARNING);
+        } else {
+            for (int i = 0; i < allAccounts.size(); i++) {
+                if (login.equals(allAccounts.get(i).getUserName()) && password.equals((allAccounts.get(i).getUserPassword()))) {
+                    main.showMainView();
+                    break;
+                } else {
+                    if (i == allAccounts.size() - 1) {
+                        Toast.makeText("Неверное имя пользователя или пароль").show(Toast.ToastType.ERROR);
+                    }
+                }
+            }
+        }
     }
 }
+
