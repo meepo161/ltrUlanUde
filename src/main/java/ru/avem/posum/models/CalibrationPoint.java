be5f84cb5ff11253c94801bc8b52db71328944a8
Ace <acerodstin@gmail.com>
2019-04-21 10:39:14 +0300

    Calibration point refactored

--- src/main/java/ru/avem/posum/models/CalibrationPoint.java
+++ src/main/java/ru/avem/posum/models/CalibrationPoint.java
@@ -1,51 +1,43 @@
 package ru.avem.posum.models;
 
 import javafx.collections.ObservableList;
-import ru.avem.posum.controllers.CalibrationController;
-import ru.avem.posum.db.models.Calibration;
 import ru.avem.posum.utils.Utils;
 
 import java.util.ArrayList;
 
 public class CalibrationPoint {
-    private int channel;
+    private int channelNumber;
     private String loadValue;
     private String channelValue;
     private String valueName;
 
-    public CalibrationPoint(int channel, CalibrationModel calibrationModel) {
+    public CalibrationPoint(int channelNumber, CalibrationModel calibrationModel) {
         double loadValue = calibrationModel.getLoadValue();
         double channelValue = calibrationModel.getChannelValue();
-        String valueName = calibrationModel.getValueName();
         int decimalFormatScale = calibrationModel.getDecimalFormatScale();
 
-        this.channel = channel;
+        this.channelNumber = channelNumber;
         this.loadValue = Utils.convertFromExponentialFormat(loadValue, decimalFormatScale);
         this.channelValue = Utils.convertFromExponentialFormat(channelValue, decimalFormatScale);
-        this.valueName = valueName;
+        this.valueName = calibrationModel.getValueName();
+        ;
     }
 
     public static ArrayList<String> toString(ObservableList<CalibrationPoint> points) {
         ArrayList<String> convertedList = new ArrayList<>();
 
         for (CalibrationPoint point : points) {
-            String channel = String.valueOf(point.getChannel());
-            String loadValue = String.valueOf(point.getLoadValue());
-            String channelValue = String.valueOf(point.getChannelValue());
-            String valueName = String.valueOf(point.getValueName());
+            String settings = String.format("Channel: %d, load value: %s, channel value: %s, value name: %s",
+                    point.getChannelNumber(), point.getLoadValue(), point.getChannelValue(), point.getValueName());
 
-            String settings = "Channel: " + channel +
-                    ", load value: " + loadValue +
-                    ", channel value: " + channelValue +
-                    ", value name: " + valueName;
             convertedList.add(settings);
         }
 
         return convertedList;
     }
 
-    public int getChannel() {
-        return channel;
+    public int getChannelNumber() {
+        return channelNumber;
     }
 
     public String getChannelValue() {
